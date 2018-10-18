62c45dd21846ad6499b99b33809907c31821ccc9
RaquelGlez <gonzalez.c.raquel@gmail.com>
2018-10-18 12:06:54 -0500

    Updates

---
+++ precache-manifest.615ad6fd3068088160ec2d7646561bc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "db3000449ee3cfd6d8bb",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/runtime~main.db300044.js"
+  },
+  {
+    "revision": "6e10adea6583317f2ba9",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/main.6e10adea.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "6e10adea6583317f2ba9",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/css/main.690ab42f.chunk.css"
+  },
+  {
+    "revision": "7a06fcc0670b0757b6c211dbea77a76c",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/index.html"
+  }
+];
