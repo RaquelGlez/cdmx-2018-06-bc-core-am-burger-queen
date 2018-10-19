e5f7bbc464ac80f5a95deffe8062fe4a2a41c035
RaquelGlez <gonzalez.c.raquel@gmail.com>
2018-10-19 06:09:36 -0500

    Updates

---
+++ precache-manifest.bebba702c07513d941d434eaec4bc4af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "25728321fe6a47e01be25b9d7e59c5a0",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/media/logo.25728321.png"
+  },
+  {
+    "revision": "db3000449ee3cfd6d8bb",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/runtime~main.db300044.js"
+  },
+  {
+    "revision": "19432c1902be126560a7",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/main.19432c19.chunk.js"
+  },
+  {
+    "revision": "ed5cc03396c101da7495",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/1.ed5cc033.chunk.js"
+  },
+  {
+    "revision": "19432c1902be126560a7",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/css/main.8a99cfeb.chunk.css"
+  },
+  {
+    "revision": "3f5a24c556653557bcc9112a2179008c",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/cdmx-2018-06-bc-core-am-burger-queen/precache-manifest.615ad6fd3068088160ec2d7646561bc4.js"
+  "/cdmx-2018-06-bc-core-am-burger-queen/precache-manifest.bebba702c07513d941d434eaec4bc4af.js"
 );
 
 workbox.clientsClaim();
